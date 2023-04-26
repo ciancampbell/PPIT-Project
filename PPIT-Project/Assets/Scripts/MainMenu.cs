b8601e58a4d3a90ef3616f8b25136eb9dddde902
ciancampbell <g00371347@atu.ie>
2023-04-26 20:47:10 +0100

    Player 2 Option

--- PPIT-Project/Assets/Scripts/MainMenu.cs
+++ PPIT-Project/Assets/Scripts/MainMenu.cs
@@ -14,4 +14,9 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void TwoPlayerGame()
+    {
+        SceneManager.LoadScene(3);
+    }
 }
--- PPIT-Project/Assets/Scripts/TwoPlayer.cs
+++ PPIT-Project/Assets/Scripts/TwoPlayer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TwoPlayers : MonoBehaviour
+public class TwoPlayer : MonoBehaviour
 {
     public void PlayGame()
     {
